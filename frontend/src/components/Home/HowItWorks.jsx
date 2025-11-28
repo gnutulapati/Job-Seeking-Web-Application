2a17bb3cacf973abd8d15a117d524859cd57ed23
Gourav Nutulapati <gourav.nutulapati@gmail.com>
2025-11-28 21:14:20 +0530

    Refactored UI: Interactive Background, Dark Mode Fixes, MyJobs Grid, Home Interactivity

--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaUserPlus } from "react-icons/fa";
 import { MdFindInPage } from "react-icons/md";
 import { IoMdSend } from "react-icons/io";
+import { Context } from "../../main";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { API_URL } from "../../config";
 
 const HowItWorks = () => {
+  const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+  const navigateTo = useNavigate();
+
+  const handleCreateAccount = async () => {
+    if (isAuthorized) {
+      try {
+        const response = await axios.get(`${API_URL}/api/v1/user/logout`, {
+          withCredentials: true,
+        });
+        toast.success(response.data.message);
+        setIsAuthorized(false);
+        navigateTo("/register");
+      } catch (error) {
+        toast.error(error.response.data.message);
+        setIsAuthorized(false);
+        navigateTo("/register");
+      }
+    } else {
+      navigateTo("/register");
+    }
+  };
+
+  const handleMiddleCard = () => {
+    if (user && user.role === "Employer") {
+      navigateTo("/job/post");
+    } else {
+      navigateTo("/job/getall");
+    }
+  };
+
+  const handleLastCard = () => {
+    navigateTo("/job/getall");
+  };
+
   return (
     <>
       <div className="howitworks">
         <div className="container">
           <h3>How JobZee Works</h3>
           <div className="banner">
-            <div className="card">
+            <div className="card" onClick={handleCreateAccount}>
               <FaUserPlus />
               <p>Create Account</p>
               <p>
@@ -18,7 +57,7 @@ const HowItWorks = () => {
                 your journey.
               </p>
             </div>
-            <div className="card">
+            <div className="card" onClick={handleMiddleCard}>
               <MdFindInPage />
               <p>Find a Job/Post a Job</p>
               <p>
@@ -26,7 +65,7 @@ const HowItWorks = () => {
                 find top talent.
               </p>
             </div>
-            <div className="card">
+            <div className="card" onClick={handleLastCard}>
               <IoMdSend />
               <p>Apply For Job/Recruit Suitable Candidates</p>
               <p>
